f3aa775e20131ff5fd32b3edd4aa56c0a4ddc7ff
333Abe <adrianbennett333@gmail.com>
2023-05-17 09:59:30 +0100

    add Topic page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,7 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page that shows all the topics
+    path('topics/', views.topics, name='topics'),
 ]
+
